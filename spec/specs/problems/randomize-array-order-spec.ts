8d34b38c3c74da34f84721c0709b43ac41ef0db4
Jason Kaczmarsky <jkaczmarsky@gmail.com>
2018-05-02 20:01:23 -0400

    Add randomizeArrayOrderInPlace

--- spec/specs/problems/randomize-array-order-spec.ts
+++ spec/specs/problems/randomize-array-order-spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="@types/jasmine" />
 
-import { randomizeArrayOrder } from "../../../src/problems/randomize-array-order";
+import { randomizeArrayOrder, randomizeArrayOrderInPlace } from "../../../src/problems/randomize-array-order";
 import * as helpers from "../../helpers/";
 
 /* tslint:disable:no-magic-numbers */
@@ -16,6 +16,22 @@ describe("randomizeArrayOrder", () => {
 
   it("Contains the same elements", () => {
     const answer: number[] = randomizeArrayOrder(helpers.cloneArray(ar));
+
+    expect(answer.every((e: number) => ar.indexOf(e) !== -1)).toEqual(true);
+  });
+});
+
+describe("randomizeArrayOrderInPlace", () => {
+  const ar: number[] = [1, 2, 3];
+
+  it("Contains the same element count", () => {
+    const answer: number[] = randomizeArrayOrderInPlace(helpers.cloneArray(ar));
+
+    expect(answer.length).toEqual(ar.length);
+  });
+
+  it("Contains the same elements", () => {
+    const answer: number[] = randomizeArrayOrderInPlace(helpers.cloneArray(ar));
 
     expect(answer.every((e: number) => ar.indexOf(e) !== -1)).toEqual(true);
   });
--- src/problems/randomize-array-order.ts
+++ src/problems/randomize-array-order.ts
@@ -14,4 +14,16 @@ function randomizeArrayOrder<T>(ar: T[]): T[] {
   return ret;
 }
 
-export { randomizeArrayOrder };
+/**
+ * Re-orders the elements in the array in a random order without creating another array
+ */
+function randomizeArrayOrderInPlace<T>(ar: T[]): T[] {
+  // tslint:disable-next-line:prefer-for-of
+  for (let i: number = 0; i < ar.length; i++) {
+    ar.push(ar.splice(Math.floor(Math.random() * ar.length), 1)[0]);
+  }
+
+  return ar;
+}
+
+export { randomizeArrayOrder, randomizeArrayOrderInPlace };
