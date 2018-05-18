b278e1e2984ce71dc6ca0b806612287461203790
Jason Kaczmarsky <jkaczmarsky@gmail.com>
2018-05-17 23:03:51 -0400

    Improve speed of justifyText by always multiplying the spaces and only adding extras via looping when necessary - About 30% speed improvement

--- src/www/problems/justify-text.ts
+++ src/www/problems/justify-text.ts
@@ -18,57 +18,31 @@ function justifyText(line: string, width: number): string {
     let sentenceWithNewlines: string = "";
 
     if (sentenceParts !== null) {
-      sentenceWithNewlines = sentenceParts.map((x: string) => justifyText(x, width).trim()).join("\n");
+      sentenceWithNewlines = sentenceParts.map((x: string) => justifyText(x, width)).join("\n");
     }
 
     return sentenceWithNewlines;
   }
 
-  const startStrLen: number = trimmedLine.length;
-  const diff: number = width - startStrLen;
   const spaceCount: number = (trimmedLine.match(/ /g) || []).length;
 
-  /*
-   * If the amount of spaces in the line is a multiple of the width
-   * then just multiply the amount of existing spaces.
-   * Optimal scenario with simple solution, return fast.
-   */
-  if (diff % spaceCount === 0) {
-    // How many spaces should be between each word
-    const multiple: number = diff / spaceCount + 1;
-
-    // Replace single space with the multiple spaces
-    return trimmedLine.split(" ").join(new Array(multiple + 1).join(" "));
-
-    // Originally had this, but during speed testing I found it was about
-    // 20% slower than the above method, when n = 1000000
-    //return trimmedLine.replace(/ /g, new Array(multiple + 1).join(" "));
+  if (spaceCount === 0) {
+    return trimmedLine;
   }
 
-  /**
-   * The rest of the function will loop through each word in the
-   * sentence and append a space to each until it has reached the
-   * desired length in order to keep the words as evenly spaced
-   * as possible.
-   */
-
-  // Split the sentence by space, maintaining the existing spaces
+  const diff: number = width - trimmedLine.length;
+  const multiple: number = Math.floor(diff / spaceCount) + 1; // How may spaces to put between each word
+  const extra: number = diff % spaceCount; // How many spaces to append bewteen words in the whole sentence
   const words: string[] = trimmedLine.split(" ");
 
-  // Loop through every word and append a new space
+  // Loop through every word and append a new space, should not loop to
   let idx: number = 0;
-  for (let i: number = 0; i < diff + spaceCount; i++) {
+  for (let i: number = 0; i < extra; i++) {
     words[idx++] += " ";
-
-    // If reached end of sentence then loop back around
-    // Ignore last word
-    if (idx >= words.length - 1) {
-      idx = 0;
-    }
   }
 
-  // Combine the parts into the full justified sentence
-  return words.join("");
+  // Combine the parts by joining with an equal number of space between each word
+  return words.join(new Array(multiple + 1).join(" "));
 }
 
 export { justifyText };
